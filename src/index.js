d322dcc8a8ab973516b7b7244ddd4bb89da22958
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2024-11-10 22:27:40 +0100

    rest task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { startServer } from './server.js';
+import { initMongoDB } from './db/initMongoDB.js';
 
 const bootstrap = async () => {
+  //спочатку підєднуємось до бази
+  await initMongoDB();
+  //потім запускаємо сервер
   startServer();
 };
 bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-
 import { env } from './utils/env.js';
-//читає файл .env: ключ=значення
-//dotenv.config();
+import * as contactServices from './services/contacts.js';
 
-//const port = Number(process.env.PORT);
-
-//функція запуску сервера
 export const startServer = () => {
   const app = express();
+
+  app.use(express.json());
 
   app.use(cors());
   const logger = pino({
@@ -18,18 +15,46 @@ export const startServer = () => {
       target: 'pino-pretty',
     },
   });
-  // app.use(logger);
+  app.use(logger);
 
-  app.get('/', (req, res) => {
+  // app.get('/', (req, res) => {
+  //   res.json({ message: 'Server is running' });
+  // });
+
+  app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getContacts();
+
     res.json({
-      message: 'Start project',
+      status: 200,
+      message: 'Successfully found contacts',
+      data,
     });
   });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id=${id} not found`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Contact successfully found`,
+      data,
+    });
+  });
+
   app.use((req, res) => {
     res.status(404).json({
       message: `${req.url} not found`,
     });
   });
+
   app.use((error, req, res, next) => {
     res.status(500).json({
       message: error.message,
@@ -38,5 +63,5 @@ export const startServer = () => {
 
   const port = Number(env('PORT', 3000));
 
-  app.listen(port, () => console.log(`Server running on ${port} PORT`));
+  app.listen(port, () => console.log(`Server running on ${port}`));
 };
