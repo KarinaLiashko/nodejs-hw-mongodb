cda4da0338ee3303740880baf97e1263e9f146d6
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2025-01-05 15:11:19 +0100

    errorHandler

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,14 +1,20 @@
-import createHttpError from 'http-errors';
-
 export const notFoundHandler = (req, res, next) => {
-   res.status(404).json({
-    message: 'Not found',
-   });
- next(createHttpError(404, 'Route not found'));
+  res.status(404).json({
+    message: 'Route not found',
+  });
 };
 
+//import createHttpError from 'http-errors';
+
+//export const notFoundHandler = (req, res, next) => {
+// res.status(404).json({
+//  message: 'Not found',
+// });
+//next(createHttpError(404, 'Route not found'));
+//};
+
 //export const notFoundHandler = (req, res) => {
- // res.status(404).json({
- //   message: `Url not found`,
- // });
+// res.status(404).json({
+//   message: `Url not found`,
+// });
 //};
